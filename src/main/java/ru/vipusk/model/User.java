9968bcfdac515b06c0da1b747aeb77ede1d55234
IHOR PETROV <Basicprof@gmail.com>
2019-05-07 19:10:27 +0300

    1.0 RestRestaurant -> logging SLF4J

--- src/main/java/ru/vipusk/model/User.java
+++ src/main/java/ru/vipusk/model/User.java
@@ -3,25 +3,23 @@ package ru.vipusk.model;
 
 public class User extends AbstractNameEntity {
 
-      private boolean        isVotingToDey;
+    private boolean isVotingToDey;
 
-       public boolean isNew(){
-             return isVotingToDey;
-       }
+    public boolean isNew() {
+        return isVotingToDey;
+    }
 
-     public User(Integer id, String name) {
-            super(id, name);
-      }
+    public User(Integer id, String name) {
+        super(id, name);
+    }
 
-      public boolean isVotingToDey() {
-            return isVotingToDey;
-      }
+    public boolean isVotingToDey() {
+        return isVotingToDey;
+    }
 
-      public void setVotingToDey(boolean votingToDey) {
-            isVotingToDey = votingToDey;
-      }
-
-
+    public void setVotingToDey(boolean votingToDey) {
+        isVotingToDey = votingToDey;
+    }
 
 
 }
--- src/main/java/ru/vipusk/rest/RestRestaurant.java
+++ src/main/java/ru/vipusk/rest/RestRestaurant.java
@@ -1,5 +1,7 @@
 package ru.vipusk.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.vipusk.model.Menu;
 import ru.vipusk.model.Restaurant;
 import ru.vipusk.model.User;
@@ -9,53 +11,67 @@ import ru.vipusk.repository.RepositoryMenagerRestaurant;
 import java.time.LocalDate;
 import java.util.List;
 
-import static ru.vipusk.model.util.ValidationUtil.checkNew;
 import static ru.vipusk.model.util.ValidationUtil.checkNotFoundWithId;
 
 public class RestRestaurant {
+    private static final Logger LOG = LoggerFactory.getLogger(RestRestaurant.class);
     private RepositoryMenagerRestaurant repositoryMenagerRestaurant = new RepositoryMenagerRestaurant();
 
 
     public void newVote(Integer idRest) {
-
-        repositoryMenagerRestaurant.newVote(idRest, SecurityUtil.authUserId());
+        int userId = SecurityUtil.authUserId();
+        LOG.info("get User {} voting Restaurant {}", userId, idRest);
+        repositoryMenagerRestaurant.newVote(idRest, userId);
 
     }
 
     public Integer isVoteToDey(Integer idRest) {
+        LOG.info("isVoteToDey  for Restaurant {}", idRest);
         return repositoryMenagerRestaurant.isVoteToDey(idRest);
     }
 
     public List<Restaurant> getAll() {
+        LOG.info("getAll  Restaurants  ");
+
         return repositoryMenagerRestaurant.getRestaurants();
     }
 
     public Restaurant getRestaurant(Integer idRest) {
+
+        LOG.info("get  Restaurant {}", idRest);
         return repositoryMenagerRestaurant.getRestaurant(idRest);
     }
 
     public Menu getMenu(int idRest, int idMenu) {
+        LOG.info("getMenu  {} for Restaurant {}", idMenu, idRest);
         return checkNotFoundWithId(repositoryMenagerRestaurant.getRestaurant(idRest).getMenu(LocalDate.now()), idMenu);
 
     }
 
     public void updateMenu(int idRest, Menu menu) {
-
+        int adminId = SecurityUtil.authUserId();
+        LOG.info("updateMenu  {} for Restaurant {} Admin {}", menu.getId(), idRest, adminId);
         repositoryMenagerRestaurant.getRestaurant(idRest).updateMenu(menu);
     }
 
     public void createMenu(int idRest, Menu menu) {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("createMenu  {} for Restaurant {} User {}", menu.getId(), idRest, userId);
 
         repositoryMenagerRestaurant.setMenu(idRest, menu);
 
     }
 
     public void createRestaurant(Restaurant restaurant) {
+        int adminId = SecurityUtil.authUserId();
+        LOG.info("createRestaurant  {} Admin {}", restaurant.getId(), adminId);
 
         repositoryMenagerRestaurant.setRestaurants(restaurant);
     }
 
     public void setUser(User user) {
+        int adminId = SecurityUtil.authUserId();
+        LOG.info("setUser  {} Admin {}", user.getId(), adminId);
 
         repositoryMenagerRestaurant.getUsers().add(user);
 
